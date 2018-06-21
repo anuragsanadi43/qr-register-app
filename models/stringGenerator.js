fe5d058d621b1b293f528d81e6b564c5b6afb2b4
Anurag Sanadi <anuragsanadi>
2018-06-20 22:25:22 -0700

    comments removed

--- models/stringGenerator.js
+++ models/stringGenerator.js
@@ -2,7 +2,3 @@ module.exports.generateCode = function(first, last) {
 	const code = first.toLowerCase() + last.toLowerCase();
 	return code;
 }
-
-
-// A lot of comments added
-// A lot of comments added
