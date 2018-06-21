011c7ab95d7b661593dc36e996594a456e371505
anuragsanadi <anurag.sanadi@gmail.com>
2018-06-20 22:23:15 -0700

    callback parameter removed

--- models/stringGenerator.js
+++ models/stringGenerator.js
@@ -1,4 +1,4 @@
-module.exports.generateCode = function(first, last, callback) {
+module.exports.generateCode = function(first, last) {
 	const code = first.toLowerCase() + last.toLowerCase();
 	return code;
 }
