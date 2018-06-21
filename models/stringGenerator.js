e1eb1544b9ea4041874e978406241596730210bb
anuragsanadi <anurag.sanadi@gmail.com>
2018-06-20 22:24:43 -0700

    Update stringGenerator.js

--- models/stringGenerator.js
+++ models/stringGenerator.js
@@ -2,3 +2,7 @@ module.exports.generateCode = function(first, last) {
 	const code = first.toLowerCase() + last.toLowerCase();
 	return code;
 }
+
+
+// A lot of comments added
+// A lot of comments added
